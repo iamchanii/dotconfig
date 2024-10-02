8c1fb0f2621e63f03d3c47feaaab26f0bc8b9a59
Chanhee Lee <contact@imch.dev>
2024-10-02 23:30:32 +0900

    Add <leader>qq keymap
    stole from lazyVim

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -7,4 +7,6 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
