b11d3a17025f6dd9e55a71023ef6188b9c969683
Chanhee Lee <contact@imch.dev>
2024-10-02 22:01:15 +0900

    Update

--- nix/brews.nix
+++ nix/brews.nix
@@ -1,5 +1,4 @@
-[
-  "starship"
-  "gh"
-  "pinentry"
-]
+# Before you add a brew here;
+# Did you check home-manager option search or nixpkgs search?
+# 
+[] # Are you read comment?
--- nix/home.nix
+++ nix/home.nix
@@ -4,29 +4,47 @@
 {
   home.stateVersion = "24.11";
 
-  home.sessionVariables = {
-    EDITOR = "vim";
-  };
-
-  # home.file.".zshrc".source = ./dotfiles/zshrc;
-
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      switch = "darwin-rebuild switch --flake ~/.config/nix#${user}";
-      vim = "nvim";
+  programs = {
+    zsh = {
+      enable = true;
+      shellAliases = {
+        switch = "darwin-rebuild switch --flake ~/.config/nix#${user}";
+      };
+      initExtra = (builtins.readFile ./dotfiles/zshrc);
     };
-    initExtra = (builtins.readFile ./dotfiles/zshrc);
-  };
 
-  programs.home-manager.enable = true;
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+      nix-direnv.enable = true;
+    };
+
+    home-manager = {
+      enable = true;
+    };
+
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    gh = {
+      enable = true;
+    };
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+  };
 
   home.packages =
     [ 
-      pkgs.vim
-      pkgs.neovim
       pkgs.nodejs_22
       pkgs.pnpm
       pkgs.cargo
+      pkgs.pinentry_mac
     ];
 }
