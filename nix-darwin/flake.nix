d3eff9d395bd4aa2e82d39c8f7d27fd888560ddb
Ette (에테) <ette@daangn.com>
2024-10-02 15:52:47 +0900

    Update

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -1,76 +1,29 @@
 {
-  description = "Example Darwin system flake";
+  description = "My system configuration";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
-  let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        [ 
-          pkgs.vim
-	        pkgs.neovim
-          pkgs.nodejs_22
-          pkgs.pnpm
-          pkgs.cargo
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
+    let
+      user = "iamchanii";
+    in
+    {
+      darwinConfigurations.${user} = nix-darwin.lib.darwinSystem {
+        modules = [
+          ./configuration.nix
         ];
-      
-      environment.shellAliases = {
-        vim = "nvim";
-        "update-system" = "darwin-rebuild switch --flake ~/.config/nix-darwin#iamchanii";
       };
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
-      # nix.package = pkgs.nix;
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-
-      # Create /etc/zshrc that loads the nix-darwin environment.
-      programs.zsh.enable = true;  # default shell on catalina
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 5;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-
-      system.defaults.NSGlobalDomain.KeyRepeat = 2; # 키 반복 속도: 빠르게
-      system.defaults.NSGlobalDomain.InitialKeyRepeat = 15; # 반복 지연 시간: 짧게
-
-      # Homebrew
-      homebrew.enable = true;
-
-      homebrew.brews = [
-        "starship"
-        "gh"
-        "pinentry"
-      ];
-
-      homebrew.casks = [
-        "wezterm"
-        "arc"
-        "raycast"
-        "rectangle"
-      ];
+      darwinPackages = self.darwinConfigurations.${user}.pkgs;
     };
-  in
-  {
-    darwinConfigurations."iamchanii" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
-    };
-
-    darwinPackages = self.darwinConfigurations."iamchanii".pkgs;
-  };
 }
