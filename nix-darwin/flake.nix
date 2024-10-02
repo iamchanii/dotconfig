1f5c24e3910513437db4629ed17251ca06353f21
Ette (에테) <ette@daangn.com>
2024-10-02 15:55:38 +0900

    Update

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -16,11 +16,33 @@
   outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
     let
       user = "iamchanii";
+
+      configuration = { pkgs, ... }: {
+        # Auto upgrade nix package and the daemon service.
+        services.nix-daemon.enable = true;
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Create /etc/zshrc that loads the nix-darwin environment.
+        programs.zsh.enable = true;  # default shell on catalina
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 5;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
     in
     {
       darwinConfigurations.${user} = nix-darwin.lib.darwinSystem {
         modules = [
-          ./configuration.nix
+          configuration
+          ./darwin.nix
         ];
       };
 
