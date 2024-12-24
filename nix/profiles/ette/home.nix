7606f72c0cabec2fc6da0e484abb95502cd5566e
Ette <ette@daangn.com>
2024-12-24 14:37:00 +0900

    Update

--- nix/profiles/ette/home.nix
+++ nix/profiles/ette/home.nix
@@ -4,9 +4,9 @@ let
   home = inputs.home-manager.users.iamchanii;
 in
 {
-  homebrew.casks = import ../casks.nix ++ [];
+  homebrew.casks = import ../../casks.nix ++ [];
 
-  homebrew.taps = import ../taps.nix ++ ["tailcallhq/tailcall"];
+  homebrew.taps = import ../../taps.nix ++ ["tailcallhq/tailcall"];
 
-  homebrew.brews = import ../brews.nix ++ ["tailcall"];
+  homebrew.brews = import ../../brews.nix ++ ["tailcall"];
 }
--- nix/profiles/iamchanii/home.nix
+++ nix/profiles/iamchanii/home.nix
@@ -4,14 +4,14 @@ let
   home = inputs.home-manager.users.ette;
 in
 {
-  homebrew.casks = import ../casks.nix ++ [
+  homebrew.casks = import ../../casks.nix ++ [
     "slack"
     "google-chrome"
     "1password"
     "orbstack"
   ];
 
-  homebrew.taps = import ../taps.nix ++ [];
+  homebrew.taps = import ../../taps.nix ++ [];
 
-  homebrew.brews = import ../brews.nix ++ [];
+  homebrew.brews = import ../../brews.nix ++ [];
 }
