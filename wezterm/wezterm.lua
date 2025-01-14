78b81a6d590fe3f0992c79f36a12dc141d530670
Chanhee Lee <contact@imch.dev>
2025-01-14 22:44:25 +0900

    Change font, update galmuri license

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.color_scheme = "Catppuccin Mocha (Gogh)"
 
 config.font_dirs = { "fonts" }
-config.font_size = 14
+config.font_size = 12
 config.font = wezterm.font_with_fallback({
 	"Geist Mono",
 	"GalmuriMono11",
