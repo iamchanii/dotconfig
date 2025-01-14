39e00ac82e65731280f6e820db5892e186e186ae
Ette <ette@daangn.com>
2025-01-14 09:37:46 +0900

    Add font config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,4 +4,11 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha (Gogh)"
 
+config.font_dirs = { "fonts" }
+config.font_size = 14
+config.font = wezterm.font_with_fallback({
+	"Geist Mono",
+	"GalmuriMono11",
+})
+
 return config
