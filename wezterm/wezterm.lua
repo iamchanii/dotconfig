a51543df9b7747045afd9b437d6688b6d160adfb
Ette <ette@daangn.com>
2025-02-25 10:01:41 +0900

    Increase font-size (aging curve) and change color scheme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,10 +2,10 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha (Gogh)"
+config.color_scheme = "Catppuccin Frappe"
 
 config.font_dirs = { "fonts" }
-config.font_size = 12
+config.font_size = 16
 config.font = wezterm.font_with_fallback({
 	"Geist Mono",
 	"GalmuriMono11",
